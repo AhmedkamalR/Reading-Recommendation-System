d0a5800cf09a621d2b1ef1d3d1f714bcb6fc0855
Ahmed Kamal <ahmed.kamal@everestminds.com>
2023-07-05 02:30:57 +0200

    implement controller logic for reading recommendation system

---
+++ src/controllers/books.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { BookService } from '../services/books.service';
+
+@Controller('books')
+export class BooksController {
+  constructor(private readonly booksService: BookService) {}
+
+  // @Get()
+  // async getAllBooks(): Promise<any> {
+  //   return this.booksService.getTopFiveBooks();
+  // }
+}
