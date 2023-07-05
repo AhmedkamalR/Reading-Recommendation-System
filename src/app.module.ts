072982c6dd90b897e9c351df0a119faec7e6e807
Ahmed Kamal <ahmed.kamal@everestminds.com>
2023-07-05 02:40:23 +0200

    update app.modules and main.ts with the logic and connect db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReadingRecommendationController } from '../src/controllers/reading-recommendation.controller';
+import { UserService } from '../src/services/users.service';
+import { UserRepository } from '../src/repositories/users.repository';
+import { BookService } from '../src/services/books.service';
+import { BookRepository } from '../src/repositories/books.repository';
+import { ReadingIntervalService } from '../src/services/reading-intervals.service';
+import { ReadingIntervalRepository } from '../src/repositories/reading-intervals.repository';
+import { User } from '../src/entities/user.entity';
+import { Book } from '../src/entities/book.entity';
+import { ReadingInterval } from '../src/entities/reading-interval.entity';
+import ormconfig from '../config/orm.config.development';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot(ormconfig),
+    TypeOrmModule.forFeature([User, Book, ReadingInterval]),
+  ],
+  controllers: [ReadingRecommendationController],
+  providers: [
+    UserService,
+    UserRepository,
+    BookService,
+    BookRepository,
+    ReadingIntervalService,
+    ReadingIntervalRepository,
+  ],
 })
 export class AppModule {}
